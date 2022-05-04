e523bc461bfde5d9dc20326adadf0d125dc2315b
Ted Ward <teddyward@gmail.com>
2022-05-04 17:58:57 +0100

    responsive nav added

--- main.js
+++ main.js
@@ -129,3 +129,10 @@ spans.forEach((span, idx) => {
 		span.classList.add('active');
 	}, 300 * (idx+10))
 });
+
+
+
+var tglbtn = document.getElementById("nav-toggle");
+var navlst = document.getElementById("nav-list")
+
+tglbtn.addEventListener('click', () => {navlst.classList.toggle("active");});
